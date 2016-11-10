20743336c4d45242394b5366f7eb9668f78fe2ed
vladygk <vladygk8@gmail.com>
2016-11-10 03:00:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ComparingFloat
+{
+    class Comparing
+    {
+        static void Main()
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double eps = 0.000001;
+            double diff = a - b;
+          double difference=  Math.Abs(diff);
+            if (difference < eps)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+
+            }
+            
+ 
+        }
+    }
+}
